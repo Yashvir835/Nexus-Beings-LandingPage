33a5ec0cac27106e7b2c2f276e097e83754cdb49
https://github.com/Yashvir835/Prakartik-Ahaar <singhyashvir835@gmail.com>
2025-01-31 23:21:39 +0530

    remove the import image in root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-    <h1>
-      hi this is yashvir malik
-    </h1>
-  </div>
+    <div>
+      <h1>
+        hi this is yashvir malik
+      </h1>
+    </div>
   );
 }
